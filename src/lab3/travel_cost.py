67c00a9042dd68ccb3b8f85ae7a3535213bd4969
PeytonOBoyle <117214395+PeytonOBoyle@users.noreply.github.com>
2023-01-27 00:54:37 -0500

    get_route_cost Complete

--- src/lab3/travel_cost.py
+++ src/lab3/travel_cost.py
@@ -8,6 +8,60 @@ In this lab, you will write a function that calculates the cost of a route betwe
 A terrain is generated for you 
 '''
 import numpy as np
+import networkx as nx
+
+#Python3 program for Bresenhams Line Generation
+#Code found on https://www.geeksforgeeks.org/bresenhams-line-generation-algorithm/
+#Contributed by akashish__, modified by me on lines 28-31
+def plotPixel(x1, y1, x2, y2, dx, dy, decide):
+   
+  # pk is initial decision making parameter
+  # Note:x1&y1,x2&y2, dx&dy values are interchanged
+  # and passed in plotPixel function so
+  # it can handle both cases when m>1 & m<1
+  pk = 2 * dy - dx
+  
+  pathList = []
+   
+  # for (int i = 0; i <= dx; i++) {
+  for i in range(0,dx+1):
+    if (decide == 0):
+      pathList.append((x1, y1))
+    else:
+      pathList.append((y1, x1))
+     
+    # checking either to decrement or increment the
+    # value if we have to plot from (0,100) to (100,0)
+    if(x1<x2):
+      x1 = x1 + 1
+    else:
+      x1 = x1 - 1
+    if (pk < 0):
+       
+      # decision value will decide to plot
+      # either  x1 or y1 in x's position
+      if (decide == 0):
+         
+        # putpixel(x1, y1, RED);
+        pk = pk + 2 * dy
+      else:
+         
+        #(y1,x1) is passed in xt
+        # putpixel(y1, x1, YELLOW);
+        pk = pk + 2 * dy
+    else:
+      if(y1<y2):
+        y1 = y1 + 1
+      else:
+        y1 = y1 - 1
+         
+      # if (decide == 0):
+      #   # putpixel(x1, y1, RED)
+      # else:
+      #   #  putpixel(y1, x1, YELLOW);
+      pk = pk + 2 * dy - 2 * dx
+      
+  return pathList
 
 def get_route_cost(route_coordinate, game_map):
     """
@@ -39,7 +93,24 @@ def get_route_cost(route_coordinate, game_map):
     :return: a floating point number representing the cost of the route
     """
     # Build a path from start to end that looks like [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 4)]
-    pass 
+    
+    x1 = route_coordinate[0][0]
+    y1 = route_coordinate[0][1]
+    x2 = route_coordinate[1][0]
+    y2 = route_coordinate[1][1]
+
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    
+    if (dx > dy):
+      # passing argument as 0 to plot(x,y)
+      path = plotPixel(x1, y1, x2, y2, dx, dy, 0)
+ 
+    # if slope is greater than or equal to 1
+    else:
+      # passing argument as 1 to plot (y,x)
+      path = plotPixel(y1, x1, y2, x2, dy, dx, 1)
+
     return game_map[tuple(zip(*path))].sum()
 
 
@@ -74,6 +145,7 @@ def main():
     print(f'Map size: {game_map.shape}')
 
     city_locations = get_randomly_spread_cities(map_size, n_cities)
+
     routes = get_routes(city_names)
     np.random.shuffle(routes)
     routes = routes[:10]
