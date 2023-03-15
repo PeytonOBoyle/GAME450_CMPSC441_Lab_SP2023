46e076c26f003449983c63ee3219d4dfab96d35b
PeytonOBoyle <117214395+PeytonOBoyle@users.noreply.github.com>
2023-03-14 23:50:10 -0400

    Lab 10 (Model Improvement not complete)

--- src/lab10/lab10.py
+++ src/lab10/lab10.py
@@ -10,7 +10,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("src/lab8/heart.csv")
+data = pd.read_csv("src/lab10/heart.csv")
 
 # Transform the categorical variables into dummy variables.
 print(data.head())
@@ -26,12 +26,18 @@ x_train, x_test, y_train, y_test = train_test_split(
 
 """ Train a sklearn model here. """
 
-sklearn_model = None
+sklearn_model = KNeighborsClassifier(n_neighbors = 8)
+sklearn_model.fit(x_train, y_train)
 
 # Accuracy
 print("Accuracy of model: {}\n".format(sklearn_model.score(x_test, y_test)))
 
 
 """ Improve the model by normalizing the input data. """
+# sklearn_model = KNeighborsClassifier(n_neighbors = 5, weights = 'distance', algorithm = 'ball_tree', leaf_size = 15)
+y_pred = sklearn_model.predict(x_test)
+cm = confusion_matrix(y_test, y_pred, normalize = 'all')
+sklearn_model.fit(x_train, y_train)
+print(cm)
 
 print("Accuracy of improved model: {}\n".format(sklearn_model.score(x_test, y_test)))
