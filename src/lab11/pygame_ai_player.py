268ce5747f00c2bb16fee038af20e0a53d8dc6e2
PeytonOBoyle <117214395+PeytonOBoyle@users.noreply.github.com>
2023-03-26 16:27:11 -0400

    Lab 10 Both AI Classes Complete

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,43 @@
+import pygame
+from lab11.turn_combat import CombatPlayer
+
 """ Create PyGameAIPlayer class here"""
 
-
+#As for now, the AI Player will move from locations 1-9 in sequential order
 class PyGameAIPlayer:
-    pass
+    def __init__(self) -> None:
+        #Start at 0, no cities visited
+        self.next_city = 0
+
+    def selectAction(self, state):
+        #If we are not travelling, move to the next city
+        if (state.travelling == False):
+            self.next_city += 1
+            print(self.next_city)
+            return ord(str(self.next_city))
+        
+        #Otherwise, continue on our path to the next city
+        return ord(str(state.current_city))
+        
 
 
 """ Create PyGameAICombatPlayer class here"""
 
+#As for now, fight the Enemy by using the fastest combination against them (Arrow on turns 1-6 8-10 and sword on turn 7)
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
+        #Count turns to counter AI pattern
+        self.turn = 0
 
-class PyGameAICombatPlayer:
-    pass
+    def weapon_selecting_strategy(self):
+        #Use Arrow when it isn't turn 6
+        if 6 < self.turn or self.turn < 6:
+            self.weapon = 1
+        #Switch to Sword
+        else:
+            self.weapon = 0
+        
+        self.turn += 1
+
+        return self.weapon
